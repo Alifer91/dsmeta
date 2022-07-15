5c8f32fbe1e36f3bad42759b3727f70c03bf7d9c
Alifer Da Silva <aliferdasilva91@gmail.com>
2022-07-15 14:23:58 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,10 +1,18 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 /* é um componente necessario por acessar o banco como o crud*/
 	
+	//essa função foi criada personalizada para consultar duas datas ao mesmo tempo ja que por padrão o jpa não faz
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -13,8 +17,19 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate,String maxDate,Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());/*dois parametros, um pra indicar que é o horario de agora, e o segundo é indicando o fuso horario, no caso é o padrão do sistema*/
+		
+		//o trecho apos esses comentarios servem para converter esse tipo de data para localdate
+		
+				//minDate.equals("")? significa max date é igual a ""?
+				//caso sim o valor é o valor de hoje(no min  será data de hoje menos um ano)
+				//caso não apenas converta a data do localDate
+				//isso serve para não acontecer erros
+		LocalDate min =minDate.equals("")? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max =maxDate.equals("")? today : LocalDate.parse(maxDate);
+		/* o jpa não possui uma função que consulta duas datas ao mesmo tempo , para isso foi preciso criar uma no arquivo sale repository */
+		return repository.findSales(min,max,pageable);
 	}
 
 }
